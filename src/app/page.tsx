c44a0224ad357a615946fea870d94e38fb9e2c98
Yashraj Deshmukh <yashrajdeshmukh@eng.rizvi.edu.in>
2024-06-02 21:08:04 +0530

    page.tsx issue resolved

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,11 +3,7 @@ import Navbar from "@/components/Navbar";
 import SideBar from "@/components/SideBar";
 import Image from "next/image";
 
-export default function Home({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
+export default function Home() {
   return (
     <>
     <Landing/>
