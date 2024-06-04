c405a1f5f379f441d4341f5cf7adc74d0f0f3a61
Yashraj Deshmukh <yashrajdeshmukh@eng.rizvi.edu.in>
2024-06-04 17:35:44 +0530

    landing page ui

--- src/app/mines/page.tsx
+++ src/app/mines/page.tsx
@@ -19,13 +19,13 @@ const mines = () => {
 
     const handleMineClick = (index: number) => {
         if(distinctNumbers.includes(index)) {
-            alert("mine is clicked");
+            
         }
     }
 
     const handleDiamondClick = (index: number) => {
         if(!distinctNumbers.includes(index)) {
-            alert("yay you clicked on a diamond 🎊")
+            
         }
     }
 
